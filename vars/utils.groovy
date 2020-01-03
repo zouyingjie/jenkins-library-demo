721ecf785931c0075ae030d62b29037891c9a315
zouyingjie <yingjiezou1@gmail.com>
2020-01-03 12:05:22 +0800

    Update Library

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,10 +1,14 @@
+import groovy.json.JsonSlurper
 
 def info(message) {
     echo "INFO: ${message}"
 }
 
 def loadJson() {
-    def config = libraryResource 'config.json'
-    echo "${config}"
-    echo "${config.config.minio}"
+    def config_text = libraryResource 'config.json'
+    def jsonSlurper = new JsonSlurper()
+    def config_object = jsonSlurper.parseText(config_text)
+    echo "${config_object}"
+    echo "${config_object.config.minio}"
+
 }
